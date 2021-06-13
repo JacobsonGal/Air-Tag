5958b91058769986c356f0cf577c059106c15a74
JacobsonGal <jacobsongal@gmail.com>
2021-06-13 20:14:42 +0300

    map works

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart';
 import 'dart:io';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'src/locations.dart' as locations;
 
 void main() {
@@ -110,7 +111,7 @@ class _GMAPState extends State<GMAP> {
   // LatLng _center = const LatLng(0,0);
   LatLng _center = const LatLng(32.0853, 34.7818);
   Future<void> _onMapCreated(GoogleMapController controller) async {
-    final airTags = await locations.getGoogleOffices();
+    // final airTags = await locations.getGoogleOffices();
     // setState(() {
     //   _markers.clear();
     //   for (final tag in airTags.offices) {
@@ -125,40 +126,110 @@ class _GMAPState extends State<GMAP> {
     //     _markers[tag.name] = marker;
     //   }
     // });
+    _getAllTags();
   }
 
-  _makeGetRequest() async {
-    final url = Uri.parse(_localhost());
-    Response response = await get(url);
+  _getAllTags() async {
+    final url = Uri.parse(
+        'http://localhost:8000/graphql?query={locations{id,name,address,lat,lng}}');
+    var response = await get(url);
     Map<String, dynamic> data = jsonDecode(response.body);
-    var location = data['data']['location'];
-
-    String name = location['name'];
-    String address = location['address'];
-    var lat = location['lat'];
-    var lng = location['lng'];
-
+    var locations = data['data']['locations'];
     setState(() {
       _markers.clear();
+      for (final tag in locations) {
+        print(tag);
         final marker = Marker(
-          markerId: MarkerId(name),
-          position: LatLng(lat, lng),
+          markerId: MarkerId(tag['name']),
+          position: LatLng(tag['lat'], tag['lng']),
           infoWindow: InfoWindow(
-            title: name,
-            snippet: address,
+            title: tag['name'],
+            snippet: tag['address'] +
+                "\n" +
+                tag['lat'].toString() +
+                " | " +
+                tag['lng'].toString(),
           ),
         );
-        _markers[name] = marker;
+        _markers[tag['name']] = marker;
+      }
     });
   }
 
-  String _localhost() {
-    if (Platform.isAndroid)
-      return 'http://10.0.2.2:8000';
-    else // for iOS simulator{}
-      return 'http://localhost:8000/graphql?query={location{name,address,lat,lng}}';
-  }
+  SpeedDial buildSpeedDial() {
+    return SpeedDial(
+      /// both default to 16
+      marginEnd: 18,
+      marginBottom: 20,
+      // animatedIcon: AnimatedIcons.menu_close,
+      // animatedIconTheme: IconThemeData(size: 22.0),
+      /// This is ignored if animatedIcon is non null
+      icon: Icons.add,
+      activeIcon: Icons.remove,
+      // iconTheme: IconThemeData(color: Colors.grey[50], size: 30),
 
+      /// The label of the main button.
+      // label: Text("Open Speed Dial"),
+      /// The active label of the main button, Defaults to label if not specified.
+      // activeLabel: Text("Close Speed Dial"),
+      /// Transition Builder between label and activeLabel, defaults to FadeTransition.
+      // labelTransitionBuilder: (widget, animation) => ScaleTransition(scale: animation,child: widget),
+      /// The below button size defaults to 56 itself, its the FAB size + It also affects relative padding and other elements
+      buttonSize: 56.0,
+      visible: true,
+
+      /// If true user is forced to close dial manually
+      /// by tapping main button and overlay is not rendered.
+      closeManually: false,
+      curve: Curves.bounceIn,
+      overlayColor: Colors.black,
+      overlayOpacity: 0.5,
+      onOpen: () => print('OPENING DIAL'),
+      onClose: () => print('DIAL CLOSED'),
+      tooltip: 'Speed Dial',
+      heroTag: 'speed-dial-hero-tag',
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      elevation: 8.0,
+      shape: CircleBorder(),
+
+      // orientation: SpeedDialOrientation.Up,
+      // childMarginBottom: 2,
+      // childMarginTop: 2,
+      gradientBoxShape: BoxShape.circle,
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [Colors.black, Colors.white],
+      ),
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.accessibility),
+          backgroundColor: Colors.red,
+          label: 'First',
+          labelStyle: TextStyle(fontSize: 18.0),
+          onTap: () => print('FIRST CHILD'),
+          onLongPress: () => print('FIRST CHILD LONG PRESS'),
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.brush),
+          backgroundColor: Colors.blue,
+          label: 'Second',
+          labelStyle: TextStyle(fontSize: 18.0),
+          onTap: () => print('SECOND CHILD'),
+          onLongPress: () => print('SECOND CHILD LONG PRESS'),
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.keyboard_voice),
+          backgroundColor: Colors.green,
+          label: 'Third',
+          labelStyle: TextStyle(fontSize: 18.0),
+          onTap: () => print('THIRD CHILD'),
+          onLongPress: () => print('THIRD CHILD LONG PRESS'),
+        ),
+      ],
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -170,24 +241,28 @@ class _GMAPState extends State<GMAP> {
             zoom: 10,
           ),
           markers: _markers.values.toSet(),
+          compassEnabled: true,
+          indoorViewEnabled: true,
+          myLocationButtonEnabled: false,
+          trafficEnabled: true,
+          zoomControlsEnabled: true,
+          zoomGesturesEnabled: true,
+          buildingsEnabled: true,
+
         ),
       ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _makeGetRequest();
+          // _makeGetRequest();
+          _getAllTags();
         },
-        child: const Icon(Icons.map),
+        child: const Icon(Icons.settings),
         backgroundColor: Colors.blue,
+        
       ),
+    
     );
-    // return GoogleMap(
-    //   onMapCreated: _onMapCreated,
-    //   initialCameraPosition: CameraPosition(
-    //     target: _center,
-    //     zoom: 10,
-    //   ),
-    //   markers: _markers.values.toSet(),
-    // );
   }
 }
 
@@ -199,28 +274,117 @@ class BodyWidget extends StatefulWidget {
 }
 
 class BodyWidgetState extends State<BodyWidget> {
-  String serverResponse = 'Server response';
+  String locationsResponse = 'locationsResponse';
+  String locationResponse = 'locationResponse';
+  String setLocationResponse = 'setLocationResponse';
+
+  int id = 0;
+  int toBeDeletedId = 0;
+  String name = '';
+  String address = '';
+  double lat = 0.0;
+  double lng = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(32.0),
+      padding: const EdgeInsets.all(2.0),
       child: Align(
         alignment: Alignment.topCenter,
         child: SizedBox(
-          width: 200,
+          width: (MediaQuery.of(context).size.width - 50),
+          height: (MediaQuery.of(context).size.height),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               ElevatedButton(
-                child: Text('Send request to server'),
+                child: Text('Get all tags'),
                 onPressed: () {
-                  _makeGetRequest();
+                  _getAllTags();
                 },
               ),
               Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(serverResponse)),
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: Text(locationsResponse)),
+              ElevatedButton(
+                child: Text('Get tags by id'),
+                onPressed: () {
+                  _getTagByID(id);
+                },
+              ),
+              TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter a tag`s id'),
+                  onSubmitted: (String input) {
+                    setState(() {
+                      id = int.parse(input);
+                    });
+                    _getTagByID(id);
+                  }),
+              Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text(locationResponse)),
+              ElevatedButton(
+                child: Text('Delete tag by id'),
+                onPressed: () {
+                  _deleteTag();
+                },
+              ),
+                ElevatedButton(
+                child: Text('Delete all tags'),
+                onPressed: () {
+                  _deleteAllTag();
+                },
+              ),
+              TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter a tag`s id'),
+                  onSubmitted: (String input) {
+                    setState(() {
+                      toBeDeletedId = int.parse(input);
+                    });
+                    _deleteTag();
+                  }),
+              ElevatedButton(
+                child: Text('Add new tag'),
+                onPressed: () {
+                  _addTag();
+                },
+              ),
+              TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Tag name'),
+                  onChanged: (String input) {
+                    setState(() {
+                      name = input.toString();
+                    });
+                  }),
+              TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Tag address'),
+                  onChanged: (String input) {
+                    setState(() {
+                      address = input.toString();
+                    });
+                  }),
+              TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Tag lat'),
+                  onChanged: (String input) {
+                    setState(() {
+                      lat = double.parse(input);
+                    });
+                  }),
+              TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Tag lng'),
+                  onChanged: (String input) {
+                    setState(() {
+                      lng = double.parse(input);
+                    });
+                  }),
             ],
           ),
         ),
@@ -228,26 +392,61 @@ class BodyWidgetState extends State<BodyWidget> {
     );
   }
 
-  _makeGetRequest() async {
-    final url = Uri.parse(_localhost());
-    Response response = await get(url);
+  _getTagByID(id) async {
+    final url = Uri.parse(
+        'http://localhost:8000/graphql?query={location(id:${id}){id,name,address,lat,lng}}');
+    var response = await get(url);
     Map<String, dynamic> data = jsonDecode(response.body);
-    var location = data['data']['location'];
-    String name = location['name'];
-    var lat = location['lat'];
-    var lng = location['lng'];
-    print('name, ${name}!');
-    print('lat, ${lat}!');
-    print('lng, ${lng}!');
+    // var location = data['data']['location'];
+    // String name = location['name'];
+    // var lat = location['lat'];
+    // var lng = location['lng'];
+    // print('name, ${name}!');
+    // print('lat, ${lat}!');
+    // print('lng, ${lng}!');
     setState(() {
-      serverResponse = name + " " + lat.toString() + " " + lng.toString();
+      locationResponse = response.body;
     });
   }
 
-  String _localhost() {
-    if (Platform.isAndroid)
-      return 'http://10.0.2.2:8000';
-    else // for iOS simulator{}
-      return 'http://localhost:8000/graphql?query={location{name,lat,lng}}';
+  _getAllTags() async {
+    final url = Uri.parse(
+        'http://localhost:8000/graphql?query={locations{id,name,address,lat,lng}}');
+    var response = await get(url);
+    setState(() {
+      locationsResponse = response.body;
+    });
+  }
+
+  _addTag() async {
+    // final url = Uri.parse(
+    //     'http://localhost:8000/graphql?query={createLocation(name:"test",address:"test",lat:32.18,lng:34.851){id,name,address,lat,lng}}');
+    final url = Uri.parse(
+        'http://localhost:8000/graphql?query={createLocation(name:"$name",address:"$address",lat:$lat,lng:$lng){id,name,address,lat,lng}}');
+    var response = await get(url);
+    setState(() {
+      locationsResponse = response.body;
+      name = '';
+      address = '';
+      lat = 0;
+      lng = 0;
+    });
+  }
+
+  _deleteTag() async {
+    final url = Uri.parse(
+        'http://localhost:8000/graphql?query={deleteLocation(id:$toBeDeletedId){id,name,address,lat,lng}}');
+    var response = await get(url);
+    setState(() {
+      locationsResponse = response.body;
+    });
+  }
+  _deleteAllTag() async {
+    final url = Uri.parse(
+        'http://localhost:8000/graphql?query={deleteLocations{id,name,address,lat,lng}}');
+    var response = await get(url);
+    setState(() {
+      locationsResponse = response.body;
+    });
   }
 }
